a4f64254a89e07545e23425ed9bfbb9f4f978938
huazie <huazie.lgh@gmail.com>
2021-02-04 10:52:32 +0800

    huazie_java-concurrency-demo_MOD

---
+++ thread-safety-demo/src/main/java/com/huazie/flea/concurrency/threadsafety/demo2/UnsafeCountingObject.java
@@ -0,0 +1,11 @@
+package com.huazie.flea.concurrency.threadsafety.demo2;
+
+/**
+ * <p> 无状态对象 </p>
+ *
+ * @author huazie
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class UnsafeCountingObject {
+}
---
+++ thread-safety-demo/src/test/java/com/huazie/flea/concurrency/threadsafety/demo1/StatelessObjectTest.java
@@ -0,0 +1,13 @@
+package com.huazie.flea.concurrency.threadsafety.demo1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StatelessObjectTest {
+
+    @Test
+    public void run() {
+        assertTrue( false );
+    }
+}
