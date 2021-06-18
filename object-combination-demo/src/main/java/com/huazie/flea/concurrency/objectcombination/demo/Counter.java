60d8af6786e23e85a07a7f26d85066f7dbf8a16c
huazie <huazie.lgh@gmail.com>
2021-06-18 18:03:05 +0800

    huazie_新增对象的组合demo_ADD

---
+++ object-combination-demo/src/main/java/com/huazie/flea/concurrency/objectcombination/demo/Counter.java
@@ -0,0 +1,26 @@
+package com.huazie.flea.concurrency.objectcombination.demo;
+
+import com.huazie.flea.concurrency.common.GuardedBy;
+
+/**
+ * <p> 使用 Java 监视器模式的线程安全计数器 </p>
+ *
+ * @author huazie
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class Counter {
+
+    @GuardedBy("this")
+    private long value = 0;
+
+    public synchronized long getValue() {
+        return value;
+    }
+
+    public synchronized long increment() {
+        if (value == Long.MAX_VALUE)
+            throw new IllegalStateException("counter overflow");
+        return ++value;
+    }
+}
